c2054ba39b8839097df61858fd4de937b60522f9
tissyuy <yulia.tismenetski@gmail.com>
2023-01-20 20:56:56 -0600

    lesson5

--- cypress/e2e/travel-site-flights-test.cy.js
+++ cypress/e2e/travel-site-flights-test.cy.js
@@ -1,8 +1,25 @@
 ///<reference types = "Cypress"/>
-describe('The flight tab will verify the flights functionality', () => {
-    it('navigate to the flights tab', () => {
-      cy.visit('http://localhost:3000')
-      cy.get ('[data-cy="Flight"]').click ()
-    })
+describe("The flight tab will verify the flights functionality", () => {
+  const flights_tab = '[data-cy="Flight"]'
+  const origin_placeholder='[placeholder="City or airport"]';
 
-})
+  before(() => {
+    cy.visit("http://localhost:3000");
+  });
+  beforeEach(() => {
+    cy.get(flights_tab).click();
+  });
+  after(() => {
+    cy.log("after all");
+  });
+  afterEach(() => {
+    cy.log("after each"); 
+  });
+  it("navigate to the flights tab", () => {
+    cy.get(origin_placeholder).should("be.visible");
+    cy.get(flights_tab)
+      .invoke("attr", "aria-expanded")
+      .should("eq", "true");
+    cy.get(flights_tab).invoke("text").should("eq", "flight");
+  });
+});
